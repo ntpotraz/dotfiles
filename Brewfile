8b863097d98944bf733f27fa582a9ee70f5a9ecb
Nathan Potraz <60411602+ntpotraz@users.noreply.github.com>
2023-01-10 22:37:49 -0800

    Streamlined Homebrew, using Brewfile now

---
+++ Brewfile
@@ -0,0 +1,27 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Brews
+brew "bat"
+brew "exa"
+brew "fd"
+brew "httpie"
+brew "lua-language-server"
+brew "neofetch"
+brew "neovim"
+brew "python@3.10"
+brew "ripgrep"
+
+# Casks
+cask "bartender"
+cask "discord"
+cask "google-chrome"
+cask "intellidock"
+cask "iterm2"
+cask "karabiner-elements"
+cask "prusaslicer"
+cask "raycast"
+cask "shottr"
+cask "visual-studio-code"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -8,21 +8,4 @@ echo "\n
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
-brew install exa
-brew install neovim
-brew install python
-brew install ripgrep
-brew install neofetch
-
-brew install google-chrome
-brew install visual-studio-code
-brew install bartender
-brew install discord
-brew install intellidock
-brew install iterm2
-brew install shottr
-brew install karabiner-elements
-brew install prusaslicer
-brew install raycast
+brew bundle --verbose
