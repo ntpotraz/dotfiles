a4881dc9301a7094eabe31b97ca489119d0b3e9d
Nathan Potraz <60411602+ntpotraz@users.noreply.github.com>
2023-03-30 21:13:39 -0700

    Updated neovim for gruvbox

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,28 +1,32 @@
 function ColorMyPencils(color)
-  color = color or "gruvbox"
-  vim.o.background = "dark" -- or "light" for light mode
-  vim.cmd.colorscheme(color)
+	color = color or "gruvbox"
+	vim.o.background = "dark" -- or "light" for light mode
+	vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, "CursorLine", { underline = true })
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "CursorLine", { underline = true })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = true,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		operators = true,
+		comments = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
 })
 ColorMyPencils()
