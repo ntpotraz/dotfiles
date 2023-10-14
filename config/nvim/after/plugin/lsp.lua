5c4891e769759194656e054d8150dda8b74d9018
Nathan Potraz <60411602+ntpotraz@users.noreply.github.com>
2023-10-14 15:02:10 -0700

    Configed some copilot stuff

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,14 +1,14 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({
-        buffer = bufnr,
-        exclude = {
-            'K',
-        }
-    })
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({
+    buffer = bufnr,
+    exclude = {
+      'K',
+    }
+  })
 end)
 
 lsp_zero.set_sign_icons({
@@ -22,14 +22,14 @@ lsp_zero.set_sign_icons({
 
 
 lsp_zero.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+  local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'gh', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>f', function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set('n', 'gh', function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set('n', '<leader>f', function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 end)
 
 local has_words_before = function()
@@ -76,5 +76,10 @@ cmp.setup({
       c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
     }),
   },
+  sources = {
+    { name = "copilot",  group_index = 2 },
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "path",     group_index = 2 },
+    { name = "luasnip",  group_index = 2 },
+  },
 })
-
