46807beb01873614ccebd7157e3e22b5459140bc
Nathan Potraz <60411602+ntpotraz@users.noreply.github.com>
2023-11-10 16:35:40 -0800

    Don't know what i was doing with treesitter

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  --fA list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {
     "python",
     "html",
