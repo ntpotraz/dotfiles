579b11f2b3536452ba2eb201a4bc263c925fe47c
Nathan Potraz <60411602+ntpotraz@users.noreply.github.com>
2024-04-15 18:32:36 -0700

    Added a Mason keymap

--- .config/nvim/lua/cadra/config/keymaps.lua
+++ .config/nvim/lua/cadra/config/keymaps.lua
@@ -21,12 +21,7 @@ keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic me
 keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- Terminal
 keymap('t', 'kj', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Transparent Background
@@ -34,3 +29,6 @@ keymap('n', '<leader>tb', ':TransparentToggle<CR>', { silent = true })
 
 -- CodeSnap
 keymap('v', '<leader>cs', ':CodeSnap<CR>', { silent = true })
+
+-- Mason
+keymap('n', '<leader>cm', ':Mason<CR>', { silent = true, desc = '[C]all [M]ason' })
