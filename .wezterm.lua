dd681d9de4f32e94bb2078c874a3cc36157edcdb
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-08-23 16:50:46 -0700

    Updated wezterm for windows powershell

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,29 @@
 local wezterm = require 'wezterm'
 local config = {}
+local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
+local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
 
 -- Font
-config.font = wezterm.font 'IosevkaTerm Nerd Font Propo'
+config.font = wezterm.font 'Iosevka Nerd Font'
 config.font_size = 15.0
 
 -- Colorscheme
 config.color_scheme = 'Gruvbox Material (Gogh)'
 
 -- Window
-config.window_background_opacity = 0.7
 config.macos_window_background_blur = 10
-config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
+
+-- Confirm Close
+config.window_close_confirmation = 'NeverPrompt'
+
+-- Windows OS Specific settings
+if not is_darwin and not is_linux then
+    config.window_background_opacity = 0.5
+    config.win32_system_backdrop = 'Acrylic'
+    config.window_decorations = 'RESIZE'
+    config.default_prog = { 'powershell.exe' }
+end
 
 
 return config
