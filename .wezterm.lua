697a0b3275485140fed1886abaec508973dffd4c
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-08-28 12:22:56 -0700

    Trying a different wezterm colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,21 +8,25 @@ config.font = wezterm.font 'Iosevka Nerd Font'
 config.font_size = 15.0
 
 -- Colorscheme
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = 'Nightlion V2 (Gogh)'
 
 -- Window
-config.macos_window_background_blur = 10
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.7
 
 -- Confirm Close
 config.window_close_confirmation = 'NeverPrompt'
 
+-- MacOS Specific settings
+if is_darwin then
+  config.macos_window_background_blur = 10
+end
+
 -- Windows OS Specific settings
 if not is_darwin and not is_linux then
-    config.win32_system_backdrop = 'Acrylic'
-    config.default_prog = { 'powershell.exe' }
+  config.win32_system_backdrop = 'Acrylic'
+  config.default_prog = { 'powershell.exe' }
 end
 
 
