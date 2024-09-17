ea8455b8d573c0cd6d79bcbf8870e912ff58ff59
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-09-16 23:34:27 -0700

    Changed formatting of font in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,8 +4,10 @@ local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
 local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
 
 -- Font
-config.font = wezterm.font 'Iosevka Nerd Font'
-config.font_size = 15.0
+config.font = wezterm.font_with_fallback {
+  'Iosevka Nerd Font',
+}
+config.font_size = 14.0
 
 -- Colorscheme
 -- config.color_scheme = 'GruvboxDarkHard'
@@ -14,7 +16,7 @@ config.color_scheme = 'MaterialDarker'
 -- Window
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.3
 
 -- Confirm Close
 config.window_close_confirmation = 'NeverPrompt'
