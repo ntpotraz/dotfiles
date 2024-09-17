5cc0affc6a6534aba60e019be664ef5a0d9af4e9
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-09-17 10:37:07 -0700

    Changed opacity stuff, reverted color scheme
    Moved the macOS and the Windows opacity to be different as they look
    completely different with the same opacity setting

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,13 +10,14 @@ config.font = wezterm.font_with_fallback {
 config.font_size = 14.0
 
 -- Colorscheme
--- config.color_scheme = 'GruvboxDarkHard'
-config.color_scheme = 'MaterialDarker'
+config.color_scheme = 'GruvboxDarkHard'
+
+-- Makes seeing the character under the cursor impossible
+-- config.color_scheme = 'MaterialDarker'
 
 -- Window
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.3
 
 -- Confirm Close
 config.window_close_confirmation = 'NeverPrompt'
@@ -24,11 +25,13 @@ config.window_close_confirmation = 'NeverPrompt'
 -- MacOS Specific settings
 if is_darwin then
   config.macos_window_background_blur = 10
+  config.window_background_opacity = 0.7
 end
 
 -- Windows OS Specific settings
 if not is_darwin and not is_linux then
   config.win32_system_backdrop = 'Acrylic'
+  config.window_background_opacity = 0.3
   config.default_prog = { 'powershell.exe' }
 end
 
