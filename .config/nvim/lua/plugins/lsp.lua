5cea69aa46189d7b9c80104b88eca5b73cda8e2b
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-10-17 13:25:53 -0700

    Added lsp keymaps and emmet to lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,6 +19,7 @@ return {
           "html-lsp",
           "cssls",
           "ts_ls",
+          "emmet_language_server",
 
           -- Formatters & Linters
           "stylua",
@@ -45,26 +46,79 @@ return {
   config = function()
     local lsp = require("lspconfig")
 
-    lsp.lua_ls.setup({
-      filetypes = { "lua" },
-    })
+    -- Define LSP keymaps function
+    local function setup_lsp_keymaps(client, bufnr)
+      local opts = { noremap = true, silent = true, buffer = bufnr }
 
-    lsp.html.setup({
+      -- Hover documentation
+      vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+      -- Navigation
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+
+      -- Workspace management
+      vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+      vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+      vim.keymap.set("n", "<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, opts)
+
+      -- Code actions and refactoring
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+      -- Diagnostics
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+
+      -- Format
+      if client.supports_method("textDocument/formatting") then
+        vim.keymap.set("n", "<leader>f", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+      end
+    end
+
+    -- Default LSP config to apply to all servers
+    local default_config = {
+      on_attach = setup_lsp_keymaps,
+      capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    }
+
+    -- Server specific setups with defaults merged in
+    lsp.lua_ls.setup(vim.tbl_deep_extend("force", default_config, {
+      filetypes = { "lua" },
+    }))
+
+    lsp.html.setup(vim.tbl_deep_extend("force", default_config, {
       filetypes = { "html" },
       init_options = {
         provideFormatter = false,
       },
-    })
-    lsp.cssls.setup({
+    }))
+
+    lsp.emmet_language_server.setup(vim.tbl_deep_extend("force", default_config, {
+      filetypes = { "html" },
+    }))
+
+    lsp.cssls.setup(vim.tbl_deep_extend("force", default_config, {
       filetypes = { "css" },
-    })
-    lsp.ts_ls.setup({
+    }))
+
+    lsp.ts_ls.setup(vim.tbl_deep_extend("force", default_config, {
       filetypes = {
         "javascript",
         "typescript",
         "javascriptreact",
         "typescriptreact",
       },
-    })
+    }))
   end,
 }
