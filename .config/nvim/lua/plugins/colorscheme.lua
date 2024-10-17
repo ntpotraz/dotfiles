1b3495392096b96104a4b40fdc1303c7b18dbf81
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-10-17 00:08:46 -0700

    Added transparent background

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  lazy = false,
-  priority = 1000,
-  name = "gruvbox",
-  config = function()
-    vim.cmd.colorscheme("gruvbox")
-    vim.o.background = "dark"
-  end,
+
+  -- Gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    name = "gruvbox",
+    config = function()
+      vim.cmd.colorscheme("gruvbox")
+      vim.o.background = "dark"
+    end,
+  },
+
+  -- Transparent
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {},
+    lazy = false,
+    keys = {
+      vim.keymap.set("n", "<leader>tb", "<CMD>TransparentToggle<CR>", { desc = "[T]oggle Transparent [B]ackground" }),
+    },
+  },
 }
