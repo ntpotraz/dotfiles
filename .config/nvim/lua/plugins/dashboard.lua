a88dbe88c831cf5857fe47ce4789cfda59af22aa
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-10-17 14:26:58 -0700

    Merged persistence.nvim with dashboard.nvim

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,7 +1,14 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function()
-    local logo = [[
+    "nvimdev/dashboard-nvim",
+    dependencies = {
+      {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = {},
+      },
+    },
+    opts = function()
+      local logo = [[
 ______________________________________/\\\_______________________________
  _____________________________________\/\\\_______________________________
   _____________________________________\/\\\_______________________________
@@ -14,15 +21,15 @@ ______________________________________/\\\_______________________________
 ntpotraz@github 
   ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-    local opts = {
-      theme = "doom",
-      hide = {
-        statusline = false,
-      },
-      config = {
-        header = vim.split(logo, "\n"),
+      local opts = {
+        theme = "doom",
+        hide = {
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
           -- stylua: ignore
           center = {
             { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
@@ -35,30 +42,30 @@ ntpotraz@github 
             { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
             { action = "qa", desc = " Quit", icon = " ", key = "q" },
           },
-        footer = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-        end,
-      },
-    }
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
 
-    for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-      button.key_format = "  %s"
-    end
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
 
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == "lazy" then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "DashboardLoaded",
-        callback = function()
-          require("lazy").show()
-        end,
-      })
-    end
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
 
-    return opts
-  end,
+      return opts
+    end,
 }
