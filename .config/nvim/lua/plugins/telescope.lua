f7880c528d876cb5879b20d22af15522dc7b3058
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-12-21 19:35:31 -0800

    Fixed telescope installation
    Other way required telescope to already be installed to work properly

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,20 @@
-local builtin = require("telescope.builtin")
-local neovimFiles = function()
-  builtin.find_files({ cwd = vim.fn.stdpath("config") })
-end
-
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   event = "VimEnter",
   branch = "0.1.x",
   dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    {"'f", builtin.find_files, desc = "[F]ind files" },
-    {"'r", builtin.live_grep, desc = "Live G[r]ep" },
-    {"<leader><space>", builtin.buffers, desc = "Buffers" },
-    {"<leader>sn", neovimFiles, desc = "[S]earch [N]eovim files" },
-  },
   config = function()
+    local builtin = require("telescope.builtin")
+    local neovimFiles = function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end
+
+    vim.keymap.set('n', "'f", builtin.find_files, { desc = "[F]ind files" })
+    vim.keymap.set('n', "'r", builtin.live_grep, { desc = "Live G[r]ep" })
+    vim.keymap.set('n', "<leader><space>", builtin.buffers, { desc = "Buffers" })
+    vim.keymap.set('n', "<leader>sn", neovimFiles, { desc = "[S]earch [N]eovim files" })
+
     require("telescope").setup({
       defaults = {
         layout_strategy = "horizontal",
