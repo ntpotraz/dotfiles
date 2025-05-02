6a78b21798279179a32aa714a031d9c5325369db
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-05-01 18:16:09 -0700

    Removed entire nvim stuff actually

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-require("config.base")
--- require("config.keymaps")
--- require("config.autocmd")
--- require("config.lazy")
--- require("config.macros")
--- .config/nvim/lua/config/macros.lua
+++
@@ -1,5 +0,0 @@
-local esc = vim.api.nvim_replace_termcodes("<ESC>", true, true, true)
-
-vim.fn.setreg('l', 'yoconsole.log(`' .. esc .. 'pa: ${' .. esc .. 'pa}`)' .. esc .. '')
-vim.fn.setreg('p', 'yoprint(f"' .. esc .. 'pa: {' .. esc .. 'pa}")' .. esc .. '')
-
