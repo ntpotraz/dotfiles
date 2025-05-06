bb670dc0e337115e5a3d745832b170ec1d0708eb
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-05-06 13:02:08 -0700

    Configured snacks more. Picker, explorer, images, dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,11 +3,63 @@ return {
   priority = 1000,
   lazy = false,
   ---@type snacks.Config
+  keys = {
+    -- Picker
+    {
+      "'f",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = '[F]ind Files',
+    },
+    {
+      '<leader><space>',
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = 'Search Buffers',
+    },
+    {
+      "'r",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = 'G[r]ep Search',
+    },
+    {
+      "'c",
+      function()
+        Snacks.picker.files({ cwd = vim.fn.stdpath('config') })
+      end,
+      desc = 'Search Config Files',
+    },
+    {
+      "'h",
+      function()
+        Snacks.picker.help()
+      end,
+      desc = '[H]elp Pages',
+    },
+    {
+      "'n",
+      function()
+        Snacks.picker.notifications()
+      end,
+      desc = '[N]otification history',
+    },
+    {
+      "'e",
+      function()
+        Snacks.explorer()
+      end,
+      desc = '[E]xplorer',
+    },
+  },
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    bigfile = { enabled = true },
+
     dashboard = {
       enabled = true,
       preset = {
@@ -25,15 +77,41 @@ ntpotraz@github 
   ]],
       },
     },
+
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ['<ESC>'] = { 'close', mode = { 'n', 'i' } },
+          },
+        },
+      },
+      enabled = true,
+      sources = {
+        buffers = {
+          focus = 'list',
+        },
+        explorer = {
+          layout = {
+            layout = {
+              position = 'right',
+            },
+          },
+        },
+      },
+    },
+
+    bigfile = { enabled = true },
     explorer = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
+    image = { enabled = true },
+    git = { enabled = true },
+    -- indent = { enabled = true },
+    -- input = { enabled = true },
+    -- notifier = { enabled = true },
+    -- quickfile = { enabled = true },
+    -- scope = { enabled = true },
+    -- scroll = { enabled = true },
+    -- statuscolumn = { enabled = true },
+    -- words = { enabled = true },
   },
 }
