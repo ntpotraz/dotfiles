910f01b31fdf04c57b5b1e60775f63eac53787ec
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-09-28 20:35:00 -0700

    Fixed console.log macro

--- .config/nvim/lua/config/macros.lua
+++ .config/nvim/lua/config/macros.lua
@@ -1,5 +1,5 @@
 local esc = vim.api.nvim_replace_termcodes('<ESC>', true, true, true)
 
-vim.fn.setreg('l', "viwyoconsole.log(`<${typeof(' .. esc .. 'pa)}> ' .. esc .. 'pa: ${' .. esc .. 'pa}`);' .. esc .. '")
-vim.fn.setreg('p', 'viwyoprint(f"{type(' .. esc ..'pa)} ' .. esc ..'pa: {' .. esc ..'pa}")' .. esc ..'')
+vim.fn.setreg('p', 'viwyoprint(f"{type(' .. esc ..'pa)} [' .. esc ..'pa] -> {' .. esc ..'pa}")' .. esc ..'')
+vim.fn.setreg('l', 'viwyoconsole.log(`<${typeof(' .. esc ..  'pa)}> [' .. esc ..  'pa] -> ${' .. esc ..  'pa}`);' .. esc ..  '')
 
