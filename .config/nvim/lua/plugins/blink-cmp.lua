f45e029bea2152d9323c3dccf45a144340c0f09c
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-10-12 10:54:38 -0700

    Fixed indenting

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -9,16 +9,6 @@ return {
     keymap = {
       preset = 'default',
       ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
-      ["<Tab>"] = {
-        "snippet_forward",
-        function() -- sidekick next edit suggestion
-          return require("sidekick").nes_jump_or_apply()
-        end,
-        function() -- if you are using Neovim's native inline completions
-          return vim.lsp.inline_completion.get()
-        end,
-        "fallback",
-      },
     },
     appearance = {
       use_nvim_cmp_as_default = false,
