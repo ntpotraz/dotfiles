c0b20ef7116e5357b133e2e09a0d2e85659c10d0
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-10-22 16:45:18 -0700

    Did some zed stuff, idk

--- .config/nvim/lua/config/base.lua
+++ .config/nvim/lua/config/base.lua
@@ -61,6 +61,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- No swap files
+vim.opt.swapfile = false
+
 -- Wrap off
 vim.opt.wrap = false
 
