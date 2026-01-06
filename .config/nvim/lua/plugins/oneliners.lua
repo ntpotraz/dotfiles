0e4f477528ac3dec6e2def24340bd608c8b1cd40
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2026-01-05 19:15:27 -0800

    Added scrollEOF

--- .config/nvim/lua/plugins/oneliners.lua
+++ .config/nvim/lua/plugins/oneliners.lua
@@ -14,4 +14,7 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim', dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, ---@module 'render-markdown', ---@type render.md.UserConfig, opts = {},
   },
+  {
+  'Aasim-A/scrollEOF.nvim', event = { 'CursorMoved', 'WinScrolled' }, opts = {},
+  },
 }
