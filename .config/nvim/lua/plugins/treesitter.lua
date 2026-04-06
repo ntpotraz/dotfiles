2e8082c98c32b929d2c3adbb35f6ab310f22555d
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2026-04-06 13:13:38 -0700

    Fixed treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+    main = 'nvim-treesitter',
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
